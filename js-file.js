5389e145ddeb051d1cc2214a4a9b4d638c1c29ce
adiferrer <adiferrer@gmail.com>
2022-05-10 16:26:52 +0800

    Add grid and needed functions

--- js-file.js
+++ js-file.js
@@ -1,13 +1,31 @@
 const container = document.querySelector('#container');
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+const newGridButton = document.querySelector('#grid-numbers');
 
-for (let i = 1; i <= 16; i++) {
-    for (let j = 1; j <= 16; j++) {
-        const squareDivs = document.createElement('div');
-        squareDivs.classList.add('square-divs');
-        container.appendChild(squareDivs);
+function createGrid(n) {
+    let div = container.querySelector('.square-divs');
+    while(div) {
+        container.removeChild(div);
+        div = container.querySelector('.square-divs');
+    }
 
-        squareDivs.addEventListener('mouseover', () =>  squareDivs.style.backgroundColor = '#BB8FCE');
-        squareDivs.addEventListener('mouseout', () =>  squareDivs.style.backgroundColor = '#BB8FCE');
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            const squareDivs = document.createElement('div');
+            squareDivs.classList.add('square-divs');
+            container.appendChild(squareDivs);
+    
+            squareDivs.addEventListener('mouseover', () =>  squareDivs.style.backgroundColor = '#BB8FCE');
+            squareDivs.addEventListener('mouseout', () =>  squareDivs.style.backgroundColor = '#BB8FCE');
+        }
     }
 }
+
+createGrid(16);
+
+newGridButton.addEventListener('click', () => {
+    let number = Number(prompt("Enter the number of rows and columns for the new square grid", "16"));
+    if (number != null && number > 0 && number <= 100) {
+        createGrid(number);
+    }
+})
